b762d8551665286b283fefecf8d49bd5a0b4a61b
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2018-05-07 16:12:06 +1000

    WR#291190 - add required core changes to get a new filter working

--- user/filters/lib.php
+++ user/filters/lib.php
@@ -28,6 +28,7 @@ require_once($CFG->dirroot.'/user/filters/date.php');
 require_once($CFG->dirroot.'/user/filters/select.php');
 require_once($CFG->dirroot.'/user/filters/simpleselect.php');
 require_once($CFG->dirroot.'/user/filters/courserole.php');
+require_once($CFG->dirroot.'/user/filters/courserolesubcat.php');
 require_once($CFG->dirroot.'/user/filters/globalrole.php');
 require_once($CFG->dirroot.'/user/filters/profilefield.php');
 require_once($CFG->dirroot.'/user/filters/yesno.php');
@@ -64,7 +65,7 @@ class user_filtering {
 
         if (empty($fieldnames)) {
             $fieldnames = array('realname' => 0, 'lastname' => 1, 'firstname' => 1, 'username' => 1, 'email' => 1, 'city' => 1, 'country' => 1,
-                                'confirmed' => 1, 'suspended' => 1, 'profile' => 1, 'courserole' => 1, 'systemrole' => 1,
+                                'confirmed' => 1, 'suspended' => 1, 'profile' => 1, 'courserole' => 1, 'courserolesubcat' => 0,  'systemrole' => 1,
                                 'cohort' => 1, 'firstaccess' => 1, 'lastaccess' => 1, 'neveraccessed' => 1, 'timemodified' => 1,
                                 'nevermodified' => 1, 'auth' => 1, 'mnethostid' => 1, 'idnumber' => 1);
         }
@@ -142,6 +143,7 @@ class user_filtering {
             case 'suspended':   return new user_filter_yesno('suspended', get_string('suspended', 'auth'), $advanced, 'suspended');
             case 'profile':     return new user_filter_profilefield('profile', get_string('profilefields', 'admin'), $advanced);
             case 'courserole':  return new user_filter_courserole('courserole', get_string('courserole', 'filters'), $advanced);
+            case 'courserolesubcat':  return new user_filter_courserolesubcat('courserolesubcat', get_string('courserole', 'filters'), $advanced);
             case 'systemrole':  return new user_filter_globalrole('systemrole', get_string('globalrole', 'role'), $advanced);
             case 'firstaccess': return new user_filter_date('firstaccess', get_string('firstaccess', 'filters'), $advanced, 'firstaccess');
             case 'lastaccess':  return new user_filter_date('lastaccess', get_string('lastaccess'), $advanced, 'lastaccess');
